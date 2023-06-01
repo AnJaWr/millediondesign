9e424f863afba78b3f9a0fb9b29db2cec3a3c9d9
anjawr <48285666+AnJaWr@users.noreply.github.com>
2023-06-01 22:23:47 +0200

    new

---
+++ assets/js/audio.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', 'assets/audio/ThisMomentiInTime.mp3');
+
+  audioElement.addEventListener(
+    'ended',
+    function () {
+      this.play();
+    },
+    false
+  );
+
+  var isPlaying = false;
+
+  $('.play_music').on('click', function () {
+    $('i', this).toggleClass('fa-play-circle fa-stop-circle');
+
+    if (isPlaying) {
+      audioElement.pause();
+    } else {
+      audioElement.play();
+    }
+
+    audioElement.onplaying = function () {
+      isPlaying = true;
+    };
+    audioElement.onpause = function () {
+      isPlaying = false;
+    };
+  });
+});
---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+$(window).on('load', function() {
+    $('#coverScreen').hide();
+});
+
+$(document).ready(function() {
+
+
+    var type_options = new Typed('.type', {
+        strings: [
+            'Hello! Whan can we do for you? '
+        ],
+        typeSpeed: 75,
+        backDelay: 7000,
+        backSpeed: 7,
+        loop: false,
+    });
+
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (
+                isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows()
+            );
+        },
+    };
+
+    $('.navmobile').hide();
+    $('.info_owner-mobile').hide();
+
+    // mobile function
+    if (isMobile.any()) {
+        $('.navmobile').show();
+        $('.info_owner-mobile').show();
+        $('.info_owner').hide();
+        console.log('this is mobile');
+        $('#m_circle_main').hide();
+    }
+});
+var button_design = document.getElementById('hoverButton-design');
+var button_pr = document.getElementById('hoverButton-pr');
+var div_design = document.getElementById('background_layer-design');
+var div_pr = document.getElementById('background_layer-pr');
+var divMain = document.getElementById('particles-js');
+
+button_design.addEventListener('mouseover', switchToDesign);
+button_pr.addEventListener('mouseover', switchToPr);
+button_design.addEventListener('mouseout', switchToDivMain);
+button_pr.addEventListener('mouseout', switchToDivMain);
+
+function switchToDesign() {
+    // divMain.classList.add('hidden');
+    divMain.setAttribute("hidden", "hidden");
+    div_design.classList.add('visible');
+}
+
+function switchToPr() {
+    divMain.setAttribute("hidden", "hidden");
+    div_pr.classList.add('visible');
+}
+
+function switchToDivMain() {
+    // divMain.classList.remove('hidden');
+    divMain.removeAttribute("hidden")
+    div_design.classList.remove('visible');
+    div_pr.classList.remove('visible');
+}
